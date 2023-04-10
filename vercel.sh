546f7016f6ff12cd35ca731ab72b309fd5848254
TurpialCode <programasfulltech@gmail.com>
2023-04-10 18:46:56 +0200

    update vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
  
-if [[ $VERCEL_GIT_COMMIT_REF == "master"  ]] ; then 
+if [[ $VERCEL_GIT_COMMIT_REF == "master" ]]; then 
   echo "This is our main branch"
   npm run build:main
-else if [[ $VERCEL_GIT_COMMIT_REF == "staggin"  ]] ; then 
+elif [[ $VERCEL_GIT_COMMIT_REF == "staggin" ]]; then 
   echo "This is our staggin branch"
   npm run build:staggin
 else
-  echo "This is not our main branch"
+  echo "This is not our main or staggin branch"
   npm run build:development
 fi
